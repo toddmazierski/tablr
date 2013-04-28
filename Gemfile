bfdc7fa8e902127e735d85144636e3af0b6ebe05
Todd Mazierski <toddmazierski@gmail.com>
2013-04-28 19:47:25 -0400

    Adding Unicorn.

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
-source :rubygems
+source "https://rubygems.org"
 
 gem "rack", "1.2.1"
 gem "sinatra"
 gem "terminal-table", :require => "terminal-table/import"
+gem "unicorn"
 
 group :development do
   gem "capistrano"
---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+root_path = File.expand_path("../", File.dirname(__FILE__))
+temp_path = File.join(root_path, "tmp")
+log_path  = File.join(root_path, "log")
+
+worker_processes  2
+working_directory root_path
+timeout           30
+listen            File.join(temp_path, "sockets/unicorn.sock"), :backlog => 64
+pid               File.join(temp_path, "pids/unicorn.pid")
+stderr_path       File.join(log_path, "unicorn.stderr.log")
+stdout_path       File.join(log_path, "unicorn.stdout.log")
